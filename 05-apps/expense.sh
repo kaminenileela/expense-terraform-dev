1292867f3696a58a68cc997e3c1f806739bdca67
kaminenileela <kaminenileela1992@email.com>
2024-06-24 19:00:21 -0400

    expense-tf

--- 05-apps/expense.sh
+++ 05-apps/expense.sh
@@ -2,8 +2,7 @@
 # user data will get sudo access
 dnf install ansible -y
 cd /tmp
-#git clone https://github.com/kaminenileela/expense-ansible-roles.git
-git clone https://github.com/kaminenileela/expense-ansible-roles1.git
-cd expense-ansible-roles1
-ansible-playbook main.yml -e component=backend -e login_password=ExpenseApp1 
-ansible-playbook main.yml -e component=frontend 
+git clone https://github.com/kaminenileela/expense-ansible-roles.git
+cd expense-ansible-roles
+ansible-playbook main.yaml -e component=backend -e login_password=ExpenseApp1 
+ansible-playbook main.yaml -e component=frontend 
